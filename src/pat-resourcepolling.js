739153f788c87113d16afda03c2cb13b2d11c755
Johannes Raggam <thetetet@gmail.com>
2021-04-18 22:45:25 +0200

    maint: Upgrade to Patternslib v4 final - resourcepolling customizations.

--- src/pat-resourcepolling.js
+++ src/pat-resourcepolling.js
@@ -1,52 +1,35 @@
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define([
-          'jquery',
-          'pat-registry',
-          'pat-parser'
-        ], function ($, registry, Parser) {
-            return factory($, registry, Parser);
-        });
-    } else {
-        factory(root.jQuery, root.patterns, root.patterns.Parser);
-    }
-}(this, function($, registry, Parser) {
-    var parser = new Parser("resourcepolling");
+import "jquery"; // still needs jQuery for replaceWith
+import Base from "@patternslib/patternslib/src/core/base";
 
-    var resourcepolling = {
-        name: "resourcepolling",
-        trigger: ".pat-resourcepolling",
-        $els: [],
-        init: function($el, opts) {
-            var timer = null;
-            var src = $el.attr("src");
-            var image = new Image();
-            var interval = 1000;
-            image.onload = function() {
-                console.log('Image found');
-                $el.replaceWith(image);
-                if (timer) {
-                    window.clearInterval(timer);
-                }
-            };
-            image.onerror = function(){
-                console.log('Image not found');
-                if (timer) {
-                    window.clearInterval(timer);
-                }
-                timer = window.setInterval(function(){
-                    console.log('sending the request again');
-                    $el[0].src = 'spinner.gif';
-                    image.src = src;
-                }, interval);
-                interval = interval*2;
-            };
-            image.src = src;
-        },
-    };
-    registry.register(resourcepolling);
-    return resourcepolling;
-}));
+export default Base.extend({
+    name: "resourcepolling",
+    trigger: ".pat-resourcepolling",
 
+    init() {
+        const src = this.el.getAttribute("src");
+        const image = new Image();
+        let timer = null;
+        let interval = 1000;
 
+        image.onload = function () {
+            console.log("Image found");
+            this.$el.replaceWith(image);
+            if (timer) {
+                window.clearInterval(timer);
+            }
+        };
+        image.onerror = function () {
+            console.log("Image not found");
+            if (timer) {
+                window.clearInterval(timer);
+            }
+            timer = window.setInterval(function () {
+                console.log("sending the request again");
+                this.el.src = "spinner.gif";
+                image.src = src;
+            }, interval);
+            interval = interval * 2;
+        };
+        image.src = src;
+    },
+});
